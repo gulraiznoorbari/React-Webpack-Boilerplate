3659cfda4927296f75fec6206fe3980355ac81c1
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-11-02 22:26:34 +0500

    Initial Files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1> React App </h1>
+                <h4> Using Webpack and Babel with Hot Reload</h4>
+            </div>
+        );
+    }
+}
+
+export default App;
